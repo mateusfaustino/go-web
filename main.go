bbfab007708a7a76785617c803a2f5c8a7f26d6f
mateus <mateusfaustino2024@gmail.com>
2024-10-05 12:31:53 -0300

    Se conectando com o banco de dados

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Post struct {
@@ -12,7 +15,14 @@ type Post struct {
 	Body  template.HTML
 }
 
+var db, err = sql.Open("mysql", "root:@tcp(localhost)/go_blog")
+
 func main() {
+	// stmt, err := db.Prepare("Insert into posts (title, body) values(?,?)")
+	// checkErr(err)
+	// _, err = stmt.Exec("My First post", "<p>This is my first content</p>")
+	// checkErr(err)
+	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		post := Post{Id: 1, Title: "First Post", Body: template.HTML("<strong>This is my first Post!</strong>")}
 
@@ -25,4 +35,10 @@ func main() {
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
+}
+
+func checkErr(err error){
+	if err != nil {
+		panic(err)
+	}
 }
