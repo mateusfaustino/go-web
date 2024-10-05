9ee195d583403ea676c24b4fa730f74e38111488
mateus <mateusfaustino2024@gmail.com>
2024-10-04 21:08:08 -0300

    Passando valores para os templates

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Post struct {
+	Id    int
+	Title string
+	Body  template.HTML
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello world")
+		post := Post{Id: 1, Title: "First Post", Body: template.HTML("<strong>This is my first Post!</strong>")}
+
+		t := template.Must(template.ParseFiles("templates/index.html"))
+		executeTemplate := t.ExecuteTemplate(w, "index.html", post)
+
+		if err := executeTemplate; err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
+
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
